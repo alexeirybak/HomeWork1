6b580d885e6745c466f67d5f71fd7c10dff1f953
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-04-02 13:49:04 +0700

    added hw4.js

---
+++ hw4.js
@@ -0,0 +1,62 @@
+let Num = 0;
+while (Num < 2) {
+    console.log('Привет!');
+    Num++;
+}
+
+// for (let Num = 1; Num <= 5; Num++) {
+//     console.log(Num);
+//     continue;
+
+// }
+// for (let Num = 7; Num <= 22; Num++) {
+//     console.log(Num);
+
+// }
+// const obj = {
+//     'Коля': '200',
+//     'Вася': '300',
+//     'Петя': '400'
+
+// };
+// for (let key in obj) {
+// console.log(`${key} — зарплата ${obj[key]} долларов.`);
+// }
+
+
+
+
+
+// let n = 1000;
+// let num = 0;
+// do {
+//     let result = n / 2;
+//     console.log(result);
+//     n = result;
+//     num++;
+// } while (n > 50);
+// console.log(`количество интераций ${num}`);
+
+// let n = 1000;
+// let num = 0;
+// while (n > 50) {
+//     n = n / 2;
+//     console.log(n);
+//     num++;
+// }
+// console.log(`количество интераций ${num}`);
+
+
+
+// let num = Number(prompt('Введите число первой пятницы месяца'));
+// if (num >= 1 && num <= 7) {
+//     console.log(`Сегодня пятница, ${num}-е число. Необходимо подготовить отчет.`);
+//     num = num + 7;
+//     while (num <= 31 && num >= 1) {
+//         console.log(`В пятницу, ${num}-ого числа. Необходимо подготовить отчет.`);
+//         num = num + 7;
+//     }
+// } else {
+//     console.log(`Проверь календарь`);
+// }
+
